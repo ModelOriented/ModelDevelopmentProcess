0be4cdbf797ff1e586d8f571aeb28819d3c7afaf
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-07-10 22:09:35 +0200

    read_process

---
+++ R/read_process.R
@@ -0,0 +1,39 @@
+#' Read process from a config file
+#'
+#' @param file name of a file with title, phases, steps, and efforts
+#'
+#' @return list with config
+#' @export
+#'
+#' @examples
+#' config <- read_process()
+#' str(config)
+read_process <- function(file = "processes/general.txt") {
+  lines <- readLines(file)
+
+  # remove empty
+  lines <- lines[lines != ""]
+  
+  # find sections
+  sections <- grep(lines, pattern = "##")
+  
+  # Title - first section
+  title <- lines[sections[1]+1]
+  
+  ## Phases
+  phases <- lines[(sections[2]+1):(sections[3]-1)]
+  phases <- strsplit(phases, split = "[:,] ")
+  
+  ## Steps
+  steps <- lines[(sections[3]+1):(sections[4]-1)]
+  steps <- strsplit(steps, split = "[:,] ")
+  
+  ## Effort
+  effort <- lines[(sections[4]+1):length(lines)]
+  effort <- strsplit(effort, split = "[ \t]")
+  
+  list(title = title, 
+       phases = phases, 
+       steps = steps, 
+       effort = effort)
+}
