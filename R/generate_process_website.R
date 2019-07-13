7820659ec3269a96d6485cb6ad9d3b94eb964aed
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-07-13 13:39:19 +0200

    added smaller plot

--- R/generate_process_website.R
+++ R/generate_process_website.R
@@ -1,34 +1,34 @@
 #' Generate website for selected processes
 #'
 #' @param process_files vector of files names with process definitions
-#' @param website path to website scheme 
+#' @param website path to website scheme
 #' @param write_to path to html file in which the final website will be written
 #'
 #' @export
-generate_process_website <- function(process_files = list.files("inst/processes", full.names = TRUE), 
-                                     website = "inst/diagramTextButton.html",
-                                     write_to = "diagram_out.html") {
+generate_process_website <- function(process_files = list.files("inst/processes", full.names = TRUE),
+                                     website = "inst/diagramTextButtonSmall.html",
+                                     write_to = "index.html") {
   # read process_files
   processes <- lapply(process_files, read_process)
 
   # convert to jsons
   process_json <- lapply(processes, process_to_json)
-  
+
   # read website template
   website_template = paste(readLines(website), collapse = "\n")
-  
-  # impute data 
-  website_template <- gsub(website_template, 
+
+  # impute data
+  website_template <- gsub(website_template,
                            pattern = "##PROCESS_DATA##",
                            replacement = paste0(process_json, collapse = ", \n"))
-  # impute selector 
+  # impute selector
   titles <- unlist(lapply(processes, function(process) process$title))
   titles <- paste0(paste0('<text class = "select" onclick="update(', seq_along(titles), ')">',titles,'</text>'), collapse = "\n")
 
-  website_template <- gsub(website_template, 
+  website_template <- gsub(website_template,
                            pattern = "##TITLE_DATA##",
                            replacement = titles)
-  
+
   writeLines(website_template, write_to)
 }
 
--- R/process_to_json.R
+++ R/process_to_json.R
@@ -10,32 +10,32 @@
 #'   process <- read_process("inst/processes/0general.txt")
 #'   cat(process_to_json(process))
 #' }
-#' 
+#'
 process_to_json <- function(process) {
   # Phases
   phases_json <- sapply(process$phases, function(phase) {
     paste0('{"top":"',phase[1],'", "bottom":[',paste0(paste0('"', phase[-1], '"'), collapse = ", "),']}')
   })
-  phases_json <- paste0('"tbTxt": [\n', 
+  phases_json <- paste0('"tbTxt": [\n',
                         paste0(phases_json, collapse = ",\n"),
                         "\n],")
-  
+
   # Steps
   steps_json <- sapply(process$steps, function(steps) {
     paste0('{"main":"',steps[1],'", "sub":[',paste0(paste0('"', steps[-1], '"'), collapse = ", "),']}')
   })
-  steps_json <- paste0('"leftTxt": [\n', 
+  steps_json <- paste0('"leftTxt": [\n',
                         paste0(steps_json, collapse = ",\n"),
                         "\n],")
-  
+
   # Effort
   effort_json <- sapply(process$effort, function(effort) {
     paste0(paste0('{"time":"A',seq_along(effort),'","value":',effort,'}'), collapse = ", ")
   })
-  effort_json <- paste0('"bar": [\n', 
+  effort_json <- paste0('"bar": [\n',
                        paste0(paste0("[", effort_json, "]"), collapse = ",\n"),
                        "\n]")
-  
+
   paste0("{\n",
          phases_json,"\n",
          steps_json,"\n",
